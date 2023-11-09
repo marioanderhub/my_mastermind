9967714bc3ee0a8e3d6fe31266250402b8b61e45
mario anderhub <anderhu_m@qwasar.io>
2023-11-09 23:56:48 +0000

    draft implementation of reading from stdin

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #define CODE_SIZE 4
 
-int main(void) {
-    char *secret_code = "0123";
-    char *user_code = "3122";
+void evaluate(char *secret_code, char *user_code) {
     int matched[CODE_SIZE] = {0};
     int placed = 0;
     int misplaced = 0;
 
     // check for correctly placed
+    // iterate user_code and secret_code together
     for(int i = 0; i < CODE_SIZE; i++) {
         if(user_code[i] == secret_code[i]) {
             matched[i] = 1;
@@ -31,5 +31,27 @@ int main(void) {
         }
     }
     printf("placed: %d, misplaced: %d", placed, misplaced);
+}
+
+int main(void) {
+    char secret_code[5] = {'0', '1', '1', '3', '\0'};
+    char user_code[5] = {'\0'};
+    char char_read;
+    int index = 0;
+    int invalid = 0;
+    while(read(1, &char_read, 1) && char_read != '\n') {
+        if (index < CODE_SIZE) {
+            if (char_read < '0' || char_read > '8') {
+                invalid = 1;
+            }
+            user_code[index] = char_read;
+            index++;
+        }
+    }
+    if (invalid || index < CODE_SIZE) {
+        printf("Invalid input\n");
+    }
+    printf("recorded input: %s\n", user_code);
+    evaluate(secret_code, user_code);
     return 0;
 }
